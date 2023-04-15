25733e5ec3d2e1697afde930915b07b180b97944
luiszkm <luiszkm@gmail.com>
2023-04-15 16:14:24 -0300

    project finished

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,16 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { UserResgiterController } from "./controllers/userRegister.controller";
 import { SessionController } from "./controllers/session.controller";
-import { verifyJWT } from "@/middlewares/verify-jwt";
-import { UpdateLastDateController } from "./controllers/updateLastDate.controller";
-
+import { refreshController } from "./controllers/refreshToken.controller";
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', UserResgiterController)
   app.post('/sessions', SessionController)
+  
+  app.patch('/token/refresh', refreshController)
 
 
-  app.patch('/user',{
-     onRequest: verifyJWT
-  }, UpdateLastDateController)
 }
